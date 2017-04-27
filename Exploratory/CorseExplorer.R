8bda5c55ba3b27c1fa701bf000d5706aaed23d99
Gus <jguszr@gmail.com>
2017-04-27 17:46:56 -0300

    Dirsource More or less working with short files

--- Exploratory/CorseExplorer.R
+++ Exploratory/CorseExplorer.R
@@ -6,5 +6,5 @@ source("./Prepare/prepareData.R")
 allFiles <- getSourceFiles()
 
 #getting started on reading the files to a Corpus
-corps <- Corpus(DirSource("./data/final/de_DE"),
+corps_DE <- Corpus(DirSource("./data/short/",pattern = "^de_DE"),
                 readerControl = list(reader= readPlain,language="de",load=TRUE))
