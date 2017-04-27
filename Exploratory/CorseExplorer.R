2342f0875f16b9d40fae125d41a72f27d7aa32de
Gus <jguszr@gmail.com>
2017-04-27 17:59:40 -0300

    it seems to be working

--- Exploratory/CorseExplorer.R
+++ Exploratory/CorseExplorer.R
@@ -6,5 +6,6 @@ source("./Prepare/prepareData.R")
 allFiles <- getSourceFiles()
 
 #getting started on reading the files to a Corpus
+
 corps_DE <- Corpus(DirSource("./data/short/",pattern = "^de_DE"),
-                readerControl = list(reader= readPlain,language="de",load=TRUE))
+                   readerControl = list(reader= readPlain,language="de",load=TRUE))
