8bf1328c11201a5858706b17b089d6265a4a72a8
Gus <jguszr@gmail.com>
2017-05-01 10:49:46 -0300

    some extra exploration

--- Exploratory/CorseExplorer.R
+++ Exploratory/CorseExplorer.R
@@ -9,7 +9,7 @@ allFiles <- getSourceFiles()
 
 #getting started on reading the files to a Corpus
 #VCorpus do not hang when using the Inspect command
-corps_DE <- VCorpus(DirSource("./data/short/",pattern = "^de_DE"),
+corps_DE <- VCorpus(DirSource("./data/short/",pattern = "^de_DE",encoding = "UTF-8"),
                    readerControl = list(reader= readPlain,language="de",load=TRUE))
 
 #inspect example - to get the text 
@@ -35,7 +35,7 @@ dtm <- DocumentTermMatrix(corps_DE)
 #finding term frequencies 
 findFreqTerms(dtm, 5)
 #find associated terms.
-findAssocs(dtm,"kultur", 0.8)
+findAssocs(dtm,"vielen dank", 0.4)
 
 #works but it is slow - the document term matrix didin't work
 gc()
@@ -46,7 +46,16 @@ findMostFreqTerms(x = dtmRtext)
 
 #work with the new sample size
 BigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 2, max = 2))
-dtm <- DocumentTermMatrix(corps_DE, control =  list(tokenize = BigramTokenizer))
-findFreqTerms(dtm,  highfreq = 9)
-findMostFreqTerms(x = dtm)
+dtm <- DocumentTermMatrix(corps_DE, 
+                          control =  list(tokenize = BigramTokenizer,
+                                          language="german",
+                                          removeNumbers=TRUE,
+                                          stemming=TRUE,
+                                          stopwords=TRUE
+                                          )
+                          )
+
+removeSparseTerms(dtm, 0.4)
+findFreqTerms(dtm,  highfreq = 999)
+freqBigrans <- findMostFreqTerms(x = dtm)
 
