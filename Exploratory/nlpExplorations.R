5bfb97d1039134f3c4a3a04b39b28a7fa1032e1a
Gus <jguszr@gmail.com>
2017-05-24 21:11:27 -0300

    it works ! Need to change a bunch of stuff but it works !

--- Exploratory/nlpExplorations.R
+++ Exploratory/nlpExplorations.R
@@ -165,6 +165,32 @@ katz_backoff_model <- function(phrase) {
   }
   
 }
+preprocess_corpus <- function(corpus) {
+  
+  # Remove punctuation from text.
+  corpus_preprocessed <- tm_map(corpus, removePunctuation)
+  
+  # Remove numbers from text.
+  corpus_preprocessed <- tm_map(corpus_preprocessed, removeNumbers)
+  
+  # Convert text to lowercase.
+  corpus_preprocessed <-
+    tm_map(corpus_preprocessed, content_transformer(tolower))
+  
+  # Strip whitespace from text.
+  corpus_preprocessed <- tm_map(corpus_preprocessed, stripWhitespace)
+  
+  # Stem the text.
+  # corpus_preprocessed <- tm_map(corpus_preprocessed, stemDocument)
+  
+  # Remove stopwords.
+  corpus_preprocessed <-
+    tm_map(corpus_preprocessed, removeWords, stopwords("en"))
+  
+  # Return value.
+  return(corpus_preprocessed)
+  
+}
 
 
 load(file = paste0(dataPath,.Platform$file.sep,"ngrams",.Platform$file.sep,"unigramMatrix2_en_US.RData"))
