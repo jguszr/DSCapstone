2feddd7e3e266345834d0f44a5aa4df48a1900b7
Gus <jguszr@gmail.com>
2017-05-30 22:41:58 -0300

    keep working... getting better results for now

--- Exploratory/quanteda.R
+++ Exploratory/quanteda.R
@@ -1,6 +1,6 @@
 ## quanteda
 library(quanteda)
-
+require(magrittr)
 dataPath <- "./data"
 shortFiles <- "./data/short"
 sampleSize <- 50000
@@ -16,9 +16,30 @@ f2 <- "short/en_US.blogs.txt"
 c1 <- readLines(con=paste0(dataPath,.Platform$file.sep, f), skipNul = TRUE)
 c2 <- readLines(con=paste0(dataPath,.Platform$file.sep, f), skipNul = TRUE)
 
-c1 <- 
-Mcorpus <- corpus(c1) 
-Bcorpus <- corpus(c2)
+c1 <- tokenize(c1,
+               remove_numbers = TRUE, 
+               remove_symbols = TRUE, 
+               remove_separators = TRUE,
+               remove_twitter = TRUE,
+               remove_hyphens = TRUE,
+               remove_url = TRUE,
+               what = "fastestword",
+               ngrams = 2)
+
+c2 <- tokenize(c2,
+               remove_numbers = TRUE, 
+               remove_symbols = TRUE, 
+               remove_separators = TRUE,
+               remove_twitter = TRUE,
+               remove_hyphens = TRUE,
+               remove_url = TRUE,
+               what = "fastestword",
+               ngrams = 2)
+
+
+
+Mcorpus <- corpus(readLines(con=paste0(dataPath,.Platform$file.sep, f), skipNul = TRUE)) 
+Bcorpus <- corpus(readLines(con=paste0(dataPath,.Platform$file.sep, f2), skipNul = TRUE))
 
 finalCorpus <- Mcorpus + Bcorpus
 rm(Mcorpus)
@@ -35,6 +56,18 @@ tdm <-  dfm(finalCorpus,
             verbose = TRUE, 
             remove = stopwords("english"), 
             stem = TRUE, 
-            removePunct=TRUE
+            remove_punct=TRUE,
+            ngrams=2
         )
+
+tdm <- dfm(c1,remove = stopwords("english"),stem = TRUE)
+tdm <- dfm_trim(x = tdm,min_count=500)
+topfeatures(tdm)
+
+set.seed(100)
+
+textplot_wordcloud(tdm, min.freq = 100, random.order = FALSE,
+                   rot.per = .25, 
+                   colors = RColorBrewer::brewer.pal(8,"Dark2"))
+
 
